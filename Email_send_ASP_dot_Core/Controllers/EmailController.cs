60180b8ea63fd0e55836e24807c215adabf30ec3
unknown <s.m.sahal786@outlook.com>
2023-11-11 22:07:34 +0500

    asp.net core send email with 2 auth

---
+++ Email_send_ASP_dot_Core/Controllers/EmailController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MailKit.Net.Smtp;
+
+namespace Email_send_ASP_dot_Core.Controllers
+{
+    public class EmailController : Controller
+    {
+        public IActionResult Index()
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Test Project", "s.m.sahal789@gmail.com"));
+            message.To.Add(new MailboxAddress("sahal", "s.m.sahal786@outlook.com"));
+            message.Subject = "test mail";
+            message.Body = new TextPart("plain")
+            {
+                Text = "Hellow world"
+            };
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 587, false);
+                client.Authenticate("s.m.sahal789@gmail.com", "hfom icuw wphk ingj");
+                client.Send(message);
+                client.Disconnect(true);
+            };
+
+            return View();
+        }
+    }
+}
---
+++ Email_send_ASP_dot_Core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Email_send_ASP_dot_Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
